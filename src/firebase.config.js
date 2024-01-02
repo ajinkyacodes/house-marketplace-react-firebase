96802ac9fc85cef9c737e55c54711fa421bb7f35
AJINKYA <ajinkyacodes@gmail.com>
2024-01-02 18:53:33 +0530

    Firebase Config file updated

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-
+// Web App Firebase Configuration
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
