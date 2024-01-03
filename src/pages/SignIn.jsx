47347f7392c59cb8b707e47cdb33cf1ff7a9c52d
AJINKYA <ajinkyacodes@gmail.com>
2024-01-03 15:58:58 +0530

    created login with firebase and showed logged user name on profile page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({    
+  const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
@@ -20,13 +21,33 @@ function SignIn() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="pageContainer">
       <header>
         <p className="pageHeader">Welcome Back!</p>
       </header>
       <main>
-        <form>          
+        <form onSubmit={onSubmit}>
           <input
             type="email"
             className="emailInput"
