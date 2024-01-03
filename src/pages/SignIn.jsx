dd93b3d08b40260bc8c350cbc359753c94e59cd9
AJINKYA <ajinkyacodes@gmail.com>
2024-01-03 21:24:05 +0530

    Added toastify message in sign-in and sign-up pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
@@ -36,8 +37,8 @@ function SignIn() {
       if (userCredential.user) {
         navigate("/");
       }
-    } catch (error) {
-      console.log(error);
+    } catch (error) {      
+      toast.error("Invalid Credentials");
     }
   };
 
