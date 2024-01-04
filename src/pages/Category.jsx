e23f3f5eba33e9790fdc11efc5ffcc0a8f0c4e59
AJINKYA <ajinkyacodes@gmail.com>
2024-01-04 21:31:43 +0530

    listingItem new component created

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,6 +12,7 @@ import {
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 function Category() {
   const [listings, setListings] = useState(null);
@@ -52,7 +53,7 @@ function Category() {
   }, [params.categoryName]);
 
   return (
-    <div className="ctegory">
+    <div className="category">
       <header>
         <p className="pageHeader">
           {params.categoryName === "rent"
@@ -67,7 +68,11 @@ function Category() {
           <main>
             <ul className="categoryListings">
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
               ))}
             </ul>
           </main>
