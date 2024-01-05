820367791e3edd516dbb0d4b4929c0cd42fa92fd
AJINKYA <ajinkyacodes@gmail.com>
2024-01-05 17:38:09 +0530

    Listing delete functionality added on Profile page

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import deleteIcon from "../assets/svg/deleteIcon.svg";
+import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
@@ -45,7 +45,7 @@ function ListingItem({ listing, id, onDelete }) {
           </div>
         </div>
         {onDelete && (
-          <deleteIcon
+          <DeleteIcon
             className="removeIcon"
             fill="rgb(231,76,60)"
             onClick={() => onDelete(listing.id, listing.name)}
